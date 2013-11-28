e4cdf670a2347dc3e9abf7422324dbf94f843821
Professional Mole <professional.mole@yahoo.com.br>
2013-11-28 17:37:40 -0200

    Basic Tile Package

---
+++ ColChess/Tiles/Tile.py
@@ -0,0 +1,12 @@
+'''Basic Tile class
+'''
+
+class Tile:
+    def Tile(self):
+        print ok
+    
+    def __repr__(self):
+        return str(self)
+    
+    def __str__(self):
+        return "#"
