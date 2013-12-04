39b8be3c84f97aab9acf39bed8feda441b29d687
Professional Mole <professional.mole@yahoo.com.br>
2013-12-04 17:16:11 -0200

    Drafted Tile Implementation

--- inc/tile.h
+++ inc/tile.h
@@ -1,10 +1,18 @@
 #ifndef _TILE_H_
 #define _TILE_H_
 #include <commons.h>
+#include <player.h>
 
 class Tile {
+	cchess::T_tiles type;
+	Player* owner;
+	Tile();
 	public:
-		Tile();
+		Tile(Player*, cchess::T_tiles);
+		
+		void setType(cchess::T_tiles);
+		
+		void setPlayer(Player*);
 };
 
 #endif
--- src/tile.cpp
+++ src/tile.cpp
@@ -1,5 +1,21 @@
 #include <tile.h>
 
-Tile::Tile()
+Tile::Tile(): Tile(NULL, cchess::T_TERRAIN)
 {
+}
+
+Tile::Tile(Player* _player, cchess::T_tiles _type)
+{
+	owner = _player;
+	type = _type;
+}
+
+void Tile::setType(cchess::T_tiles _type)
+{
+	type = _type;
+}
+		
+void Tile::setPlayer(Player* _player)
+{
+	owner = _player;
 }
